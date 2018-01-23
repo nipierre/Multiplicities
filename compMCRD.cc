eb93eeb7e3482aae949541ed2d8a8b1a872cbf73
npierre <nicolas.pierre@cern.ch>
2018-01-23 13:16:42 +0100

    Fix.

--- compMCRD.cc
+++ compMCRD.cc
@@ -3815,8 +3815,8 @@ void RDextraction(string pFilelist)
           zBj = 0;
         }
 
-        if(0.1<zBj && (LH->GetLeaf("Hadrons.LH")->GetValue(3+6*i)==fLHsec_tab[3])
-                   && (LH->GetLeaf("Hadrons.LH")->GetValue(4+6*i)==fLHsec_tab[3]))
+        if(0.1<zBj && (LH->GetLeaf("Hadrons.LH")->GetValue(3+6*i)==fLHsec_tab[3]
+                   || LH->GetLeaf("Hadrons.LH")->GetValue(4+6*i)==fLHsec_tab[3]))
                    fKinematicsRD[0][11]->Fill(abs(ph->GetLeaf("Hadrons.ph")->GetValue(i)));
 
         // Maximum radiation length cumulated
