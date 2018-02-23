eb97801915a1e04d1796c66a78b856b7c2b6bf81
npierre <nicolas.pierre@cern.ch>
2018-02-23 16:30:45 +0100

    Fix.

--- compMCMC.cc
+++ compMCMC.cc
@@ -1374,19 +1374,8 @@ void MC1extraction(string pFilelist)
   Double_t xBj = 0;
   Double_t yBj = 0;
   Double_t zBj = 0;
-  Double_t zBj_unid = 0;
   Double_t wBj = 0;
   Double_t nu = 0;
-
-  Double_t MCE0 = 0;
-  Double_t MCE1 = 0;
-  Double_t Q2_MC = 0;
-  Double_t xBj_MC = 0;
-  Double_t yBj_MC = 0;
-  Double_t zBj_MC = 0;
-  Double_t zBj_MC_unid = 0;
-  Double_t wBj_MC = 0;
-  Double_t nu_MC = 0;
 
   // Target cells
   if(Y2012) InitTargetFile(target_file_2012);
@@ -1676,44 +1665,6 @@ void MC1extraction(string pFilelist)
       }
 
       //2006 ---
-
-      //MC
-      MCE0 = sqrt(pow(fM_mu,2)
-                  +MC_p0x->GetLeaf("MC_p0x")->GetValue()*MC_p0x->GetLeaf("MC_p0x")->GetValue()
-                  +MC_p0y->GetLeaf("MC_p0y")->GetValue()*MC_p0y->GetLeaf("MC_p0y")->GetValue()
-                  +MC_p0z->GetLeaf("MC_p0z")->GetValue()*MC_p0z->GetLeaf("MC_p0z")->GetValue());
-
-      MCE1 = sqrt(pow(fM_mu,2)
-                  +MC_p1x->GetLeaf("MC_p1x")->GetValue()*MC_p1x->GetLeaf("MC_p1x")->GetValue()
-                  +MC_p1y->GetLeaf("MC_p1y")->GetValue()*MC_p1y->GetLeaf("MC_p1y")->GetValue()
-                  +MC_p1z->GetLeaf("MC_p1z")->GetValue()*MC_p1z->GetLeaf("MC_p1z")->GetValue());
-
-      Q2_MC = 2.*( MCE0*MCE1
-           - MC_p0x->GetLeaf("MC_p0x")->GetValue()*MC_p1x->GetLeaf("MC_p1x")->GetValue()
-           - MC_p0y->GetLeaf("MC_p0y")->GetValue()*MC_p1y->GetLeaf("MC_p1y")->GetValue()
-           - MC_p0z->GetLeaf("MC_p0z")->GetValue()*MC_p1z->GetLeaf("MC_p1z")->GetValue()
-           - pow(fM_mu,2));
-
-      nu_MC = MCE0 - MCE1;
-
-      if(MCE0 != 0)
-        yBj_MC = nu_MC/MCE0;
-      else
-        yBj_MC = 0;
-
-      if(nu_MC != 0)
-      {
-        xBj_MC = Q2_MC/(2*fM_p*nu_MC);
-      }
-      else
-      {
-        xBj_MC = 0;
-      }
-
-      if(xBj_MC != 0)
-        wBj_MC = pow(fM_p,2) + Q2_MC*(1-xBj_MC)/xBj_MC;
-      else
-        wBj_MC = 0;
 
       //--------------------------------------------------------------------------
       //--------- Target ---------------------------------------------------------
@@ -3043,7 +2994,7 @@ void MC2extraction(string pFilelist)
           fXX0test++;
 
           // Momentum cut (12 GeV to 40 GeV, increasing to 3 GeV to 40 GeV)
-          if(!(MOMENTUM<p->GetLeaf("Hadrons.P")->GetValue(i) && p->GetLeaf("Hadrons.P")->GetValue(i)<40)) continue;
+          if(!(fPmin<p->GetLeaf("Hadrons.P")->GetValue(i) && p->GetLeaf("Hadrons.P")->GetValue(i)<fPmax)) continue;
           fMom++;
 
           // Theta cut
@@ -3568,7 +3519,7 @@ int main(int argc, char **argv)
   }
 
   create_kin_plots();
-  readKinCuts(argv[3])
+  readKinCuts(argv[3]);
   MC1extraction(argv[1]);
   MC2extraction(argv[2]);
   MultRatio();
