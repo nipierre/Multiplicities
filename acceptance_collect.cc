6ccbec40950608691b655bfa592d5ae317c88034
npierre <nicolas.pierre@cern.ch>
2018-04-23 18:59:26 +0200

    debug

--- acceptance_collect.cc
+++ acceptance_collect.cc
@@ -512,6 +512,8 @@ int main()
           if(!(int(k_corr.size()))) k_corr_empty = 1;
           if(!(int(h_corr.size()))) h_corr_empty = 1;
 
+          cout << "pouet" << endl;
+
           H_corr_zvtx[c][i][j][k] = new TGraphErrors(int(h_corr.size()),&(zvtx_range_h[0]),&(h_corr[0]),0,&(h_cerr[0]));
           P_corr_zvtx[c][i][j][k] = new TGraphErrors(int(p_corr.size()),&(zvtx_range_p[0]),&(p_corr[0]),0,&(p_cerr[0]));
           K_corr_zvtx[c][i][j][k] = new TGraphErrors(int(k_corr.size()),&(zvtx_range_k[0]),&(k_corr[0]),0,&(k_cerr[0]));
@@ -539,6 +541,8 @@ int main()
           H_corr_zvtx[c][i][j][k]->SetTitle("");
           P_corr_zvtx[c][i][j][k]->SetTitle("");
           K_corr_zvtx[c][i][j][k]->SetTitle("");
+
+          cout << "pouet" << endl;
 
           if(!h_corr_empty)
           {
