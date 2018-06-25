54dc36ad6e764ae7799245b5e971d54f3f62ff75
npierre <nicolas.pierre@cern.ch>
2018-06-25 23:52:12 +0200

    Test.

--- analySIDIS_split.cc
+++ analySIDIS_split.cc
@@ -273,7 +273,8 @@ Double_t GetInclusiveRadiativeCorrection(Double_t x, Double_t y)
   }
   else if(Y2012 || Y2016)
   {
-    return fInclusiveRCproton[xb][yb];
+    return 1;
+    // return fInclusiveRCproton[xb][yb];
   }
   else
   {
@@ -1247,14 +1248,14 @@ int main(int argc, char **argv)
           {
             fFlag[2][xbin][ybin][i]=1;
           }
-          if(fFlag[0][xbin][ybin][i] /*|| fFlag[1][xbin][ybin][i] || fFlag[2][xbin][ybin][i]*/)
+          if(fFlag[0][xbin][ybin][i] || fFlag[1][xbin][ybin][i] || fFlag[2][xbin][ybin][i])
           {
             fNDIS_evt[0][xbin][ybin][i] -= GetInclusiveRadiativeCorrection(xBj,yBj);
-            // fNDIS_evt[1][xbin][ybin][i] -= GetInclusiveRadiativeCorrection(xBj,yBj);
-            // fNDIS_evt[2][xbin][ybin][i] -= GetInclusiveRadiativeCorrection(xBj,yBj);
+            fNDIS_evt[1][xbin][ybin][i] -= GetInclusiveRadiativeCorrection(xBj,yBj);
+            fNDIS_evt[2][xbin][ybin][i] -= GetInclusiveRadiativeCorrection(xBj,yBj);
             fNDIS_evt_err[0][xbin][ybin][i] -= pow(GetInclusiveRadiativeCorrection(xBj,yBj),2);
-            // fNDIS_evt_err[1][xbin][ybin][i] -= pow(GetInclusiveRadiativeCorrection(xBj,yBj),2);
-            // fNDIS_evt_err[2][xbin][ybin][i] -= pow(GetInclusiveRadiativeCorrection(xBj,yBj),2);
+            fNDIS_evt_err[1][xbin][ybin][i] -= pow(GetInclusiveRadiativeCorrection(xBj,yBj),2);
+            fNDIS_evt_err[2][xbin][ybin][i] -= pow(GetInclusiveRadiativeCorrection(xBj,yBj),2);
           }
         }
 
