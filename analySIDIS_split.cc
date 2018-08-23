77119cbe85a4793953bb3e589da18f170251f8b2
npierre <nicolas.pierre@cern.ch>
2018-08-23 12:29:20 +0200

    Test.

--- analySIDIS_split.cc
+++ analySIDIS_split.cc
@@ -1190,27 +1190,27 @@ int main(int argc, char **argv)
           fFlag[2][xbin][ybin][i]=0;
 
           // nu cut
-          if(!(fNu_min[0][i]<nu && nu<fNu_max[0][i]))
-          {
-            fFlag[0][xbin][ybin][i]=1;
-          }
-          if(!(fNu_min[1][i]<nu && nu<fNu_max[1][i]))
-          {
-            fFlag[1][xbin][ybin][i]=1;
-          }
-          if(!(fNu_min[2][i]<nu && nu<fNu_max[2][i]))
-          {
-            fFlag[2][xbin][ybin][i]=1;
-          }
-          if(fFlag[0][xbin][ybin][i] || fFlag[1][xbin][ybin][i] || fFlag[2][xbin][ybin][i])
-          {
-            fNDIS_evt[0][xbin][ybin][i] -= GetInclusiveRadiativeCorrection(xBj,yBj);
-            fNDIS_evt[1][xbin][ybin][i] -= GetInclusiveRadiativeCorrection(xBj,yBj);
-            fNDIS_evt[2][xbin][ybin][i] -= GetInclusiveRadiativeCorrection(xBj,yBj);
-            fNDIS_evt_err[0][xbin][ybin][i] -= pow(GetInclusiveRadiativeCorrection(xBj,yBj),2);
-            fNDIS_evt_err[1][xbin][ybin][i] -= pow(GetInclusiveRadiativeCorrection(xBj,yBj),2);
-            fNDIS_evt_err[2][xbin][ybin][i] -= pow(GetInclusiveRadiativeCorrection(xBj,yBj),2);
-          }
+          // if(!(fNu_min[0][i]<nu && nu<fNu_max[0][i]))
+          // {
+          //   fFlag[0][xbin][ybin][i]=1;
+          // }
+          // if(!(fNu_min[1][i]<nu && nu<fNu_max[1][i]))
+          // {
+          //   fFlag[1][xbin][ybin][i]=1;
+          // }
+          // if(!(fNu_min[2][i]<nu && nu<fNu_max[2][i]))
+          // {
+          //   fFlag[2][xbin][ybin][i]=1;
+          // }
+          // if(fFlag[0][xbin][ybin][i] || fFlag[1][xbin][ybin][i] || fFlag[2][xbin][ybin][i])
+          // {
+          //   fNDIS_evt[0][xbin][ybin][i] -= GetInclusiveRadiativeCorrection(xBj,yBj);
+          //   fNDIS_evt[1][xbin][ybin][i] -= GetInclusiveRadiativeCorrection(xBj,yBj);
+          //   fNDIS_evt[2][xbin][ybin][i] -= GetInclusiveRadiativeCorrection(xBj,yBj);
+          //   fNDIS_evt_err[0][xbin][ybin][i] -= pow(GetInclusiveRadiativeCorrection(xBj,yBj),2);
+          //   fNDIS_evt_err[1][xbin][ybin][i] -= pow(GetInclusiveRadiativeCorrection(xBj,yBj),2);
+          //   fNDIS_evt_err[2][xbin][ybin][i] -= pow(GetInclusiveRadiativeCorrection(xBj,yBj),2);
+          // }
         }
 
         // -------------------------------------------------------------------------
