f4f4d65b80caef5bf8ed943020e11411642b0aff
npierre <nicolas.pierre@cern.ch>
2018-09-19 17:18:36 +0200

    Fix.

--- analySIDIS_split.cc
+++ analySIDIS_split.cc
@@ -1130,7 +1130,7 @@ int main(int argc, char **argv)
         if(!(mu_prim_chi2->GetLeaf("mu_prim_chi2")->GetValue()<10)) continue;
         fMupchi2++;
 
-        if(!(MZfirst->GetLeaf("MZfirst")->GetValue()<10)) continue;
+        if(!(MZfirst->GetLeaf("MZfirst")->GetValue()<350)) continue;
         fMZfirst++;
 
         // IM/O triggers
