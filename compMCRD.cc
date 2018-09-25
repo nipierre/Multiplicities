a5f97068fd2878c3facdacc91b010efdf7a4a880
npierre <nicolas.pierre@cern.ch>
2018-09-25 22:49:11 +0200

    Test

--- compMCRD.cc
+++ compMCRD.cc
@@ -200,7 +200,7 @@ void create_kin_plots()
     fKinematicsRD[i][14] = new TH1F(Form("phi_{hadron+e,lab} %s",trigname[i].c_str()), Form("phi_{hadron+e,lab} %s",trigname[i].c_str()), 50, -3.5, 3.5);
     fKinematicsRD[i][15] = new TH1F(Form("phi_{hadron+e,prod.pl} %s",trigname[i].c_str()), Form("phi_{hadron+e,prod.pl} %s",trigname[i].c_str()), 50, 0, 3.5);
     fKinematicsRD[i][16] = new TH1F(Form("p_{T} %s",trigname[i].c_str()), Form("p_{T} %s",trigname[i].c_str()), 50, 0, 3);
-    fKinematicsMC[i][0] = new TH1F(Form("Q^{2} Ratio %s",trigname[i].c_str()), Form("Q^{2} Ratio %s",trigname[i].c_str()), 50, -1, 2);
+    fKinematicsMC[i][0] = new TH1F(Form("Q2 Ratio %s",trigname[i].c_str()), Form("Q2 Ratio %s",trigname[i].c_str()), 50, -1, 2);
     fKinematicsMC[i][1] = new TH1F(Form("x_{Bj} Ratio %s",trigname[i].c_str()), Form("x_{Bj} Ratio %s",trigname[i].c_str()), 50, -3, 0);
     fKinematicsMC[i][2] = new TH1F(Form("y Ratio %s",trigname[i].c_str()), Form("y Ratio %s",trigname[i].c_str()), 50, 0, 1);
     fKinematicsMC[i][3] = new TH1F(Form("z Ratio %s",trigname[i].c_str()), Form("z Ratio %s",trigname[i].c_str()), 50, 0, 1);
@@ -334,7 +334,6 @@ void save_kin_plots()
 
   for(int i=0; i<4; i++)
   {
-    cout << i << endl;
     c1.cd(i+1);
     plotting_device(i,0,0);
     gPad->SetLogx();
