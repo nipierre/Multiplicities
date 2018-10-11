7c044b096a2ce2df8a1bff013cbc70699fbfa748
npierre <nicolas.pierre@cern.ch>
2018-10-11 19:02:45 +0200

    Test.

--- compMult.cc
+++ compMult.cc
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-void LoadMultiplicityFiles(string pfile1, strin pfile2)
+void LoadMultiplicityFiles(string pfile1, string pfile2)
 {
   string sdum;
   Double_t x,y,z;
@@ -91,17 +91,233 @@ void yweightedavg()
 int main(int argc, char **argv)
 {
 
-  LoadMultiplicityFiles(string argv[1], strin argv[2])
+  if(argc < 3)
+  {
+    cout << "ERROR : Not enough arguments." << endl;
+    cout << "Asked : 2 *** Received : " << argc-1 << endl;
+    cout << "./compMult Mult1 Mult2" << endl;
 
-  r.push_back(fMultiplicities2[i][j][k].tab[0][0][0] ? fMultiplicities1[i][j][k].tab[1][0][0]/fMultiplicities2[i][j][k].tab[0][0][0] : 0);
-  r_err.push_back(sqrt((fMultiplicities1[i][j][k].tab[1][1][0]+pow(fMultiplicities2[i][j][k].tab[0][1][0],2)*fMultiplicities1[i][j][k].tab[1][0][0]
-                          /pow(fMultiplicities2[i][j][k].tab[0][0][0],2))/pow(fMultiplicities2[i][j][k].tab[0][0][0],2)));
+    return 1;
+  }
 
-  yweightedavg();
+  LoadMultiplicityFiles(argv[1],argv[2]);
 
-  r_y.push_back(fMultiplicities2_yavg[i][k].tab[0][0][0] ? fMultiplicities1_yavg[i][k].tab[1][0][0]/fMultiplicities2_yavg[i][k].tab[0][0][0] : 0);
-  r_y_err.push_back(sqrt((fMultiplicities1_yavg[i][k].tab[1][1][0]+pow(fMultiplicities2_yavg[i][k].tab[0][1][0],2)*fMultiplicities1_yavg[i][k].tab[1][0][0]
-                          /pow(fMultiplicities2_yavg[i][k].tab[0][0][0],2))/pow(fMultiplicities2_yavg[i][k].tab[0][0][0],2)));
+  TCanvas c1("Mult_comparison","Mult_comparison",3200,1600);
+  TCanvas c2("Mult_comparison_yavg","Mult_comparison_yavg",3200,1600);
+  c1.SetFillColor(0);
+  c2.SetFillColor(0);
+  c1.Divide(9,5,0,0);
+  c2.Divide(5,2,0,0);
+
+  TGraphErrors* R[2][9][6];
+  TGraphErrors* R_y[2][9];
+
+  for(int i=0; i<9; i++)
+  {
+    int axisflagh1 = 0;
+    int axisflagh2 = 0;
+    int axisflagp1 = 0;
+    int axisflagp2 = 0;
+    int axisflagk1 = 0;
+    int axisflagk2 = 0;
+
+    vector<double> r_y;
+    vector<double> r_y_err;
+    vector<double> z_range_r_y;
+    
+    for(int l=0; l<12; l++)
+    {
+      z_range_r_y.push_back(z_range[l]);
+    }
+
+    for(int j=0; j<6; j++)
+    {
+      std::vector<double> r;
+      std::vector<double> r_err;
+      std::vector<double> z_range_r;
+
+      for(int l=0; l<12; l++)
+      {
+        z_range_r.push_back(z_range[l]);
+      }
+
+      for(int k=0; k<12; k++)
+      {
+        for(int c=1; c>=0; c--)
+        {
+          r.push_back(fMultiplicities2[i][j][k].tab[c][0][0] ? fMultiplicities1[i][j][k].tab[c][0][0]/fMultiplicities2[i][j][k].tab[c][0][0] : 0);
+          r_err.push_back(sqrt((fMultiplicities1[i][j][k].tab[c][1][0]+pow(fMultiplicities2[i][j][k].tab[c][1][0],2)*fMultiplicities1[i][j][k].tab[c][0][0]
+                                  /pow(fMultiplicities2[i][j][k].tab[c][0][0],2))/pow(fMultiplicities2[i][j][k].tab[c][0][0],2)));
+        }
+      }
+
+      for(int k=12; k>0; k--)
+      {
+        if(!r[k-1]) {r.erase(r.begin()+k-1); r_err.erase(r_err.begin()+k-1); z_range_r.erase(z_range_r.begin()+k-1);}
+      }
+
+      bool r_empty = 0;
+
+      if(!(int(r.size()))) r_empty = 1;
+
+      R[c][i][j] = new TGraphErrors(int(r.size()),&(z_range_r[0]),&(r[0]),0,&(r_err[0]));
+
+      if(!c)
+      {
+        R[c][i][j]->SetMarkerColor(fMarkerColor[4]);
+      }
+      else
+      {
+        R[c][i][j]->SetMarkerColor(fMarkerColor[0]);
+      }
+
+      R[c][i][j]->SetMarkerSize(3);
+
+      R[c][i][j]->SetMarkerStyle(fMarkerStyle[0][c]);
+      R[c][i][j]->GetYaxis()->SetTitle("");
+
+      R[c][i][j]->GetXaxis()->SetTitle("");
+
+      R[c][i][j]->SetTitle("");
+
+      if(!r_empty)
+      {
+        c1.cd(i+1+9*j);
+        gPad->SetFillStyle(4000);
+        if(R[c][i][j])
+        {
+          if(!c)
+          {
+            R[c][i][j]->Draw("SAMEPA");
+            R[c][i][j]->GetXaxis()->SetLimits(0.1,0.9);
+            R[c][i][j]->SetMinimum(0.);
+            R[c][i][j]->SetMaximum(2.0);
+            R[c][i][j]->GetXaxis()->SetLabelSize(0.06);
+            R[c][i][j]->GetYaxis()->SetLabelSize(0.06);
+            R[c][i][j]->SetTitle("");
+            if(j==5) gPad->SetBottomMargin(.15);
+            if(i==0) gPad->SetLeftMargin(.22);
+            if(i==8 && j==5)
+            {
+              R[c][i][j]->GetXaxis()->SetTitle("#font[ 12]{z}");
+              R[c][i][j]->GetXaxis()->SetTitleSize(0.08);
+              R[c][i][j]->GetXaxis()->SetTitleOffset(.8);
+            }
+            R[c][i][j]->GetXaxis()->SetNdivisions(304,kTRUE);
+            R[c][i][j]->GetYaxis()->SetNdivisions(304,kTRUE);
+            if(i==1 && j==0)
+            {
+              R[c][i][j]->GetYaxis()->SetTitle("#font[12]{acceptance}^{#font[ 12]{h}}");
+              R[c][i][j]->GetYaxis()->SetTitleSize(0.08);
+            }
+            R[c][i][j]->Draw("SAMEP");
+            R[c][i][j]->GetXaxis()->SetLimits(0.1,0.9);
+            R[c][i][j]->SetMinimum(0.);
+            R[c][i][j]->SetMaximum(2.0);
+            c1.Range(0.1,0.,0.9,2.0);
+          }
+          else
+          {
+            R[c][i][j]->Draw("SAMEP");
+            R[c][i][j]->GetXaxis()->SetLimits(0.1,0.9);
+            R[c][i][j]->SetMinimum(0.);
+            R[c][i][j]->SetMaximum(2.0);
+          }
+        }
+        c1.Update();
+      }
+    }
+
+    yweightedavg();
+
+    for(int k=0; k<12; k++)
+    {
+      for(int c=1; c>=0; c--)
+      {
+        r_y[c][i].push_back(fMultiplicities2_yavg[i][k].tab[c][0][0] ? fMultiplicities1_yavg[i][k].tab[c][0][0]/fMultiplicities2_yavg[i][k].tab[c][0][0] : 0);
+        r_y_err[c][i].push_back(sqrt((fMultiplicities1_yavg[i][k].tab[c][1][0]+pow(fMultiplicities2_yavg[i][k].tab[c][1][0],2)*fMultiplicities1_yavg[i][k].tab[c][0][0]
+                                /pow(fMultiplicities2_yavg[i][k].tab[c][0][0],2))/pow(fMultiplicities2_yavg[i][k].tab[c][0][0],2)));
+      }
+    }
+    for(int k=12; k>0; k--)
+    {
+      if(!r_y[k-1]) {r_y.erase(r_y.begin()+k-1); r_y_err.erase(r_y_err.begin()+k-1); z_range_r_y.erase(z_range_r_y.begin()+k-1);}
+    }
+
+    bool r_y_empty = 0;
+
+    if(!(int(r_y.size()))) r_y_empty = 1;
+
+    R_y[c][i] = new TGraphErrors(int(r_y.size()),&(z_range_r_y[0]),&(r_y[0]),0,&(r_y_err[0]));
+
+    if(!c)
+    {
+      R_y[c][i][j]->SetMarkerColor(fMarkerColor[4]);
+    }
+    else
+    {
+      R_y[c][i][j]->SetMarkerColor(fMarkerColor[0]);
+    }
+
+    R_y[c][i][j]->SetMarkerSize(3);
+
+    R_y[c][i][j]->SetMarkerStyle(fMarkerStyle[0][c]);
+    R_y[c][i][j]->GetYaxis()->SetTitle("");
+
+    R_y[c][i][j]->GetXaxis()->SetTitle("");
+
+    R_y[c][i][j]->SetTitle("");
+
+    if(!r_y_empty)
+    {
+      c2.cd(i+1);
+      gPad->SetFillStyle(4000);
+      if(R_y[c][i][j])
+      {
+        if(!c)
+        {
+          R_y[c][i][j]->Draw("SAMEPA");
+          R_y[c][i][j]->GetXaxis()->SetLimits(0.1,0.9);
+          R_y[c][i][j]->SetMinimum(0.);
+          R_y[c][i][j]->SetMaximum(2.0);
+          R_y[c][i][j]->GetXaxis()->SetLabelSize(0.06);
+          R_y[c][i][j]->GetYaxis()->SetLabelSize(0.06);
+          R_y[c][i][j]->SetTitle("");
+          if(i>4) gPad->SetBottomMargin(.15);
+          if(i==0 && i==5) gPad->SetLeftMargin(.22);
+          if(i==8)
+          {
+            R_y[c][i][j]->GetXaxis()->SetTitle("#font[ 12]{z}");
+            R_y[c][i][j]->GetXaxis()->SetTitleSize(0.08);
+            R_y[c][i][j]->GetXaxis()->SetTitleOffset(.8);
+          }
+          R_y[c][i][j]->GetXaxis()->SetNdivisions(304,kTRUE);
+          R_y[c][i][j]->GetYaxis()->SetNdivisions(304,kTRUE);
+          if(i==0)
+          {
+            R_y[c][i][j]->GetYaxis()->SetTitle("#font[12]{acceptance}^{#font[ 12]{h}}");
+            R_y[c][i][j]->GetYaxis()->SetTitleSize(0.08);
+          }
+          R_y[c][i][j]->Draw("SAMEP");
+          R_y[c][i][j]->GetXaxis()->SetLimits(0.1,0.9);
+          R_y[c][i][j]->SetMinimum(0.);
+          R_y[c][i][j]->SetMaximum(2.0);
+          c2.Range(0.1,0.,0.9,2.0);
+        }
+        else
+        {
+          R_y[c][i][j]->Draw("SAMEP");
+          R_y[c][i][j]->GetXaxis()->SetLimits(0.1,0.9);
+          R_y[c][i][j]->SetMinimum(0.);
+          R_y[c][i][j]->SetMaximum(2.0);
+        }
+      }
+      c2.Update();
+    }
+  }
+
+  c1.Print("mult_ratio.pdf");
+  c2.Print("mult_ratio_yavg.pdf");
 
   return 0;
 }
--- compMult.h
+++ compMult.h
@@ -18,5 +18,11 @@ Multiplicities fMultiplicities2_yavg[9][12];
 Multiplicities fMultiplicities1_zavg[9];
 Multiplicities fMultiplicities2_zavg[9];
 
+double z_range[12] = {.225,.275,.325,.375,.425,.475,.525,.575,.625,.675,.725,.8};
+
+//Graphic Style
+
+int fMarkerColor[6] = {2,95,209,226,4,221};
+int fMarkerStyle[6][2] = {{24,20},{26,22},{25,21},{27,33},{28,34},{30,29}};
 
 #endif
