45ca60d00df61e16e7e61ca963374e1f3825a331
npierre <nicolas.pierre@cern.ch>
2018-10-16 20:52:39 +0200

    Fix.

--- analySIDIS_collect.cc
+++ analySIDIS_collect.cc
@@ -587,7 +587,7 @@ int main(int argc, char **argv)
     {
       fetch_acceptance(Form("acceptance/%d/acceptance_%s.txt",year,periodName.c_str()),fNumberPeriod-1);
       fetch_yavg_acceptance(Form("acceptance/%d/acceptance_yavg_%s.txt",year,periodName.c_str()),fNumberPeriod-1);
-      fetch_zvtx_acceptance(Form("acceptance/%d/acceptance_zvtx_%s.txt",year,periodName.c_str()),fNumberPeriod-1);
+      fetch_zvtx_acceptance(Form("acceptance/%d/acceptance_vtx_%s.txt",year,periodName.c_str()),fNumberPeriod-1);
     }
     else
     {
