837bb9d30e988aca9ee721641cff22359c8d3b6e
npierre <nicolas.pierre@cern.ch>
2018-11-29 23:12:42 +0100

    Fixed julia applet

--- TargetStudy.jl
+++ TargetStudy.jl
@@ -1,6 +1,7 @@
 using DelimitedFiles
+using ImplicitEquations, Plots
 
-target_data = readdlm("data/target-274508-274901.dat")
+target_data = readdlm("data/target-data.dat")
 target_mc = readdlm("data/target-mc.dat")
 
 x_data = target_data[:,8]
@@ -13,12 +14,45 @@ z_mc = target_mc[:,1]
 R = 2
 d = y_data - y_mc
 sizeData = size(y_data)
-Intersection_Volume = 0
-Total_Volume = 0
+intersection_Volume = 0
+total_Volume = 0
+slice_diff = zeros(25)
+slice_cum = zeros(25)
+zred = zeros(25)
 
-for i = 1:29
-    Intersection_Volume += (2*2^2*acos(d[i]/(2*R))-(d[i]/2)*sqrt(4*R^2-d[i]^2))*abs(z_data[i]-z_data[i+1])
-    Total_Volume += 4*pi*abs(z_data[i]-z_data[i+1])
+
+for i in 1:25
+    global intersection_Volume
+    global total_Volume
+    global slice_diff
+    global slice_cum
+    global zred
+    local iV, tV
+    iV = (2*2^2*acos(abs(d[i])/(2*R))-(abs(d[i])/2)*sqrt(4*R^2-d[i]^2))*abs(z_data[i]-z_data[i+1])
+    tV = 4*pi*abs(z_data[i]-z_data[i+1])
+    intersection_Volume += iV
+    total_Volume += tV
+    slice_diff[i] = (tV-iV)/tV
+    slice_cum[i] = (total_Volume-intersection_Volume)/total_Volume
+    zred[i] = z_data[i]
 end
 
-show(Intersection_Volume/Total_Volume)
+# p1 = plot(slice_diff)
+# p2 = plot(slice_cum)
+# plot(p1,p2,lw=3,title="Lost volume in intersection (%)")
+plot(zred,slice_diff)
+plot!(zred,slice_cum)
+savefig("myplot.png")
+plot(z_data,y_data,ribbon = (1,1),fillalpha = 0.3)
+plot!(z_mc,y_mc,ribbon = (1,1),fillalpha = 0.3)
+savefig("myplot2.png")
+
+anim = @animate for i=1:26
+    f(x,y) = x^2 + (y-y_data[i])^2 - 4
+    g(x,y) = x^2 + (y-y_mc[i])^2 - 4
+    r = (Ge(f,0)) & (Le(g,0))
+    plot(r)
+end
+gif(anim, "mygif.gif", fps = 15)
+
+println((total_Volume-intersection_Volume)/total_Volume)
