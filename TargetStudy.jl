792a723579f8d9745788a06bff334d5438a185e7
npierre <nicolas.pierre@cern.ch>
2018-11-30 17:09:53 +0100

    Added Julia Snippet

--- TargetStudy.jl
+++ TargetStudy.jl
@@ -40,19 +40,40 @@ end
 # p1 = plot(slice_diff)
 # p2 = plot(slice_cum)
 # plot(p1,p2,lw=3,title="Lost volume in intersection (%)")
-plot(zred,slice_diff)
-plot!(zred,slice_cum)
+plot(zred,slice_diff, lw=3,
+                      xlabel = "z",
+                      ylabel = "%",
+                      label="Residual volume in intersection (%)")
+plot!(zred,slice_cum, lw=3,
+                      xlabel = "z",
+                      ylabel = "%",
+                      label="Cumulated residual volume in intersection (%)")
 savefig("myplot.png")
-plot(z_data,y_data,ribbon = (1,1),fillalpha = 0.3)
-plot!(z_mc,y_mc,ribbon = (1,1),fillalpha = 0.3)
+plot(z_data,y_data, ribbon = (1,1),
+                    fillalpha = 0.3,
+                    xlabel = "z",
+                    ylabel = "y",
+                    label="Data Target")
+plot!(z_mc,y_mc, ribbon = (1,1),
+                 fillalpha = 0.3,
+                 xlabel = "z",
+                 ylabel = "y",
+                 label="MC Target")
 savefig("myplot2.png")
 
 anim = @animate for i=1:26
     f(x,y) = x^2 + (y-y_data[i])^2 - 4
     g(x,y) = x^2 + (y-y_mc[i])^2 - 4
-    r = (Ge(f,0)) & (Le(g,0))
-    plot(r)
+    r = (Le(f,0)) & (Ge(g,0))
+    t = string("Residual volume in intersection at z =", z_data[i])
+    plot(r, xlabel = "x",
+            xlims = (-3,3),
+            xticks = -3:0.5:3,
+            ylabel = "y",
+            ylims = (-3,3),
+            yticks = -3:0.5:3,
+            title=t)
 end
-gif(anim, "mygif.gif", fps = 15)
+gif(anim, "mygif.gif", fps = 3)
 
 println((total_Volume-intersection_Volume)/total_Volume)
