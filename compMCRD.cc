1b98553b4dbdb086b4c479c7fc5848d4d413eec2
npierre <nicolas.pierre@cern.ch>
2019-02-18 11:57:55 +0100

    Fix.

--- compMCRD.cc
+++ compMCRD.cc
@@ -2787,8 +2787,6 @@ int main(int argc, char **argv)
     return 1;
   }
 
-  mf = new TFile("RDMC.root","recreate");
-  mf->cd();
   create_root_tree();
   create_kin_plots();
   readKinCuts(argv[3]);
@@ -2797,6 +2795,7 @@ int main(int argc, char **argv)
   cout << "... Monte-Carlo treatment ..." << endl;
   MCextraction(argv[2]);
   cout << "... Saving plots ..." << endl;
+  mf = new TFile("RDMC.root","recreate");
   DIS->Write();
   Hadron->Write();
   DISMC->Write();
