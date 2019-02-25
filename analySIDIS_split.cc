3b0ceb3baca3afae4d81d97374c635e54f47d669
npierre <nicolas.pierre@cern.ch>
2019-02-25 08:30:54 +0100

    Fix.

--- analySIDIS_split.cc
+++ analySIDIS_split.cc
@@ -1534,8 +1534,6 @@ int main(int argc, char **argv)
 
         if(Y2016)
         {
-          if(!(-325<zlab && zlab<-71)) continue;
-
           if(-325<=zlab && zlab<-261.5) zlabbin = 0;
           else if(-261.5<=zlab && zlab<-198) zlabbin = 1;
           else if(-198<=zlab && zlab<-134.5) zlabbin = 2;
