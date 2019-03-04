c566311fb50d4f1ef747cf030c54732a2907a5c9
npierre <nicolas.pierre@cern.ch>
2019-03-04 17:36:13 +0100

    WIP.

--- analySIDIS_collect.cc
+++ analySIDIS_collect.cc
@@ -2431,8 +2431,8 @@ int main(int argc, char **argv)
         }
 
         // cout << c << " " << i << " " << k << " " << fMultiplicities_yavg[i][k].tab[c][0][3] << " " << fMultiplicities_yavg[i][k].tab[c][1][3] << " " << fMultiplicities_yavg[i][k].tab[c][2][3] << endl;
-        // cout << c << " " << i << " " << k << " " << fMultiplicities_theta_yavg[i][k].tab[c][0][3] << " " << fMultiplicities_theta_yavg[i][k].tab[c][1][3] << " " << fMultiplicities_theta_yavg[i][k].tab[c][2][3] << endl;
-        cout << c << " " << i << " " << k << " " << fMultiplicities_pt_yavg[i][k].tab[c][0][3] << " " << fMultiplicities_pt_yavg[i][k].tab[c][1][3] << " " << fMultiplicities_pt_yavg[i][k].tab[c][2][3] << endl;
+        cout << c << " " << i << " " << k << " " << fMultiplicities_theta_yavg[i][k].tab[c][0][3] << " " << fMultiplicities_theta_yavg[i][k].tab[c][1][3] << " " << fMultiplicities_theta_yavg[i][k].tab[c][2][3] << endl;
+        // cout << c << " " << i << " " << k << " " << fMultiplicities_pt_yavg[i][k].tab[c][0][3] << " " << fMultiplicities_pt_yavg[i][k].tab[c][1][3] << " " << fMultiplicities_pt_yavg[i][k].tab[c][2][3] << endl;
 
         p_y[c][i].push_back(fMultiplicities_yavg[i][k].tab[c][0][0]);
         k_y[c][i].push_back(fMultiplicities_yavg[i][k].tab[c][0][1]);
