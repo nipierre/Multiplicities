074812c7ac1eb82fed037244f25c3fc924ecde2c
npierre <nicolas.pierre@cern.ch>
2019-03-04 15:10:41 +0100

    WIP.

--- analySIDIS_collect.cc
+++ analySIDIS_collect.cc
@@ -1212,10 +1212,7 @@ int main(int argc, char **argv)
                                                         *fBinning_period[period][i][j][k].tab[c][1][0][l]/(fNDIS_evt_period[period][0][1][i][j][k]*fZ_bin_width[k]*fAcceptance[period][i][j][k].tab[c][1][0][l]))
                                                         : 0);
 
-                                                        if(l==3) {cout << (fBinning_period[period][i][j][k].tab[c][1][0][l] && fNDIS_evt_period[period][0][1][i][j][k] && fAcceptance[period][i][j][k].tab[c][1][0][l] ?
-                                                                                                  (PeriodFlux[1][period]/PeriodFluxTot)*fDiffVectorMeson[c][i][j][k][l]*GetSemiInclusiveRadiativeCorrection(i,j,k+1)
-                                                                                                  *fBinning_period[period][i][j][k].tab[c][1][0][l]/(fNDIS_evt_period[period][0][1][i][j][k]*fZ_bin_width[k]*fAcceptance[period][i][j][k].tab[c][1][0][l])
-                                                                                                  : 0) << end;}
+                                                        if(l==3) {cout << fMultiplicities[i][j][k].tab[c][0][l] << end;}
 
               fMultiplicities[i][j][k].tab[c][0][l] += (fBinning_period[period][i][j][k].tab[c][0][0][l] && fNDIS_evt_period[period][0][0][i][j][k] && fAcceptance[period][i][j][k].tab[c][0][0][l] ?
                                                         Double_t((PeriodFlux[0][period]/PeriodFluxTot)*fDiffVectorMeson[c][i][j][k][l]*GetSemiInclusiveRadiativeCorrection(i,j,k+1)
