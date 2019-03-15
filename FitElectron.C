24881f31488b0650b43930455ccc7c6c8f658b42
npierre <nicolas.pierre@cern.ch>
2019-03-15 15:35:40 +0100

    Fix.Fix.

--- FitElectron.C
+++ FitElectron.C
@@ -3,8 +3,6 @@ void FitElectron(double x1, double x2, double y1, double y2)
   TFile *_file0 = TFile::Open("RDMC.root");
   TTree *Hadron = (TTree*) _file0->Get("Hadron");
 
-  double par[7] = {0,0,0,0,0,0.95,0.1};
-
   TH1F *h1 = new TH1F();
   Hadron->Draw("(EECAL1+EECAL2)/phad>>h1",Form("(PID==0 || PID==1) && isinECAL==1 && 0.1<(EECAL1+EECAL2)/phad && (EECAL1+EECAL2)/phad<1.25 && %f<xh && xh<%f && %f<yh && yh<%f",x1,x2,y1,y2));
   h1 = (TH1F*) gDirectory->Get("h1");
@@ -12,10 +10,13 @@ void FitElectron(double x1, double x2, double y1, double y2)
   TF1 *f1 = new TF1("f1","pol3+gaus(4)",0.3,1.25);
   f1->SetParameters(par);
 
-  h1->Fit("f1","R");
+  double gausCenter;
+  f1->GetParameter(&gausCenter);
+  while(!(0.9<gausCenter && gausCenter<1))
+  {
+    f1->SetParameter(5,0.95);
+    f1->SetParameter(6,0.1);
 
-  f1->SetParameter(5,0.95);
-  f1->SetParameter(6,0.1);
-
-  h1->Fit("f1","R");
+    h1->Fit("f1","R");
+  }
 }
