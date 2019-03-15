747ccfc5f9c4b56712598229c4ce30a526ffe49f
npierre <nicolas.pierre@cern.ch>
2019-03-15 11:24:39 +0100

    Fix.

--- ElectronMacro.C
+++ ElectronMacro.C
@@ -12,15 +12,15 @@ double contamination(double x1, double x2)
   TH1F *h1, *h2, *h3, *h4;
 
   Hadron->Draw("(EECAL1+EECAL2)/phad>>h1",Form("(PID==2 || PID==3) && isinECAL==1 && %f<xh && xh<%f",x1,x2));
-  RK = h1->GetEntries();
-  Hadron->Draw("(EECAL1+EECAL2)/phad>>h2",Form("(PID==2 || PID==3) && isinECAL==1 && 0.8<(EECAL1+EECAL2)/phad && (EECAL1+EECAL2)/phad<1.25 && %f<xh && xh<%f",x1,x2));
-  RK /= h2->GetEntries();
-  Hadron->Draw("(EECAL1+EECAL2)/phad>>h3",Form("(PID==0 || PID==1) && isinECAL==1 && %f<xh && xh<%f",x1,x2));
-  picount = RK*h3->GetEntries();
-  Hadron->Draw("(EECAL1+EECAL2)/phad>>h4",Form("(PID==0 || PID==1) && isinECAL==1 && 0.8<(EECAL1+EECAL2)/phad && (EECAL1+EECAL2)/phad<1.25 && %f<xh && xh<%f",x1,x2));
-  piecount = h4->GetEntries();
-
-  econt = (piecount - picount)/h3->GetEntries();
+  // RK = h1->GetEntries();
+  // Hadron->Draw("(EECAL1+EECAL2)/phad>>h2",Form("(PID==2 || PID==3) && isinECAL==1 && 0.8<(EECAL1+EECAL2)/phad && (EECAL1+EECAL2)/phad<1.25 && %f<xh && xh<%f",x1,x2));
+  // RK /= h2->GetEntries();
+  // Hadron->Draw("(EECAL1+EECAL2)/phad>>h3",Form("(PID==0 || PID==1) && isinECAL==1 && %f<xh && xh<%f",x1,x2));
+  // picount = RK*h3->GetEntries();
+  // Hadron->Draw("(EECAL1+EECAL2)/phad>>h4",Form("(PID==0 || PID==1) && isinECAL==1 && 0.8<(EECAL1+EECAL2)/phad && (EECAL1+EECAL2)/phad<1.25 && %f<xh && xh<%f",x1,x2));
+  // piecount = h4->GetEntries();
+  //
+  // econt = (piecount - picount)/h3->GetEntries();
 
   return econt;
 }
