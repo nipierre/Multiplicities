2a18a523c4f2e1781b00688832324a5202908ff4
npierre <nicolas.pierre@cern.ch>
2019-03-15 14:38:39 +0100

    Fix.

--- ElectronMacro.C
+++ ElectronMacro.C
@@ -33,6 +33,41 @@ double contamination(double x1, double x2, double y1, double y2)
   return econt;
 }
 
+double contaminationMC(double x1, double x2, double y1, double y2)
+{
+
+  double RK = 0;
+  double picount = 0;
+  double piecount = 0;
+  double econt = 0;
+
+  TFile *_file0 = TFile::Open("RDMC.root");
+  TTree *HadronMC = (TTree*) _file0->Get("HadronMC");
+
+  TH1F *h1 = new TH1F();
+  HadronMC->Draw("(EECAL1_MC+EECAL2_MC)/phad_MC>>h1",Form("(PID_MC==2 || PID_MC==3) && isinECAL_MC==1 && %f<xh_MC && xh_MC<%f && %f<yh_MC && yh_MC<%f",x1,x2,y1,y2));
+  h1 = (TH1F*) gDirectory->Get("h1");
+
+  TH1F *h2 = new TH1F();
+  HadronMC->Draw("(EECAL1_MC+EECAL2_MC)/phad_MC>>h2",Form("(PID_MC==2 || PID_MC==3) && isinECAL_MC==1 && 0.8<(EECAL1_MC+EECAL2_MC)/phad && (EECAL1_MC+EECAL2_MC)/phad_MC<1.25 && %f<xh_MC && xh_MC<%f && %f<yh_MC && yh_MC<%f",x1,x2,y1,y2));
+  h2 = (TH1F*) gDirectory->Get("h2");
+  RK = h2->GetEntries()/h1->GetEntries();;
+
+  TH1F *h3 = new TH1F();
+  HadronMC->Draw("(EECAL1_MC+EECAL2_MC)/phad_MC>>h3",Form("(PID_MC==0 || PID_MC==1 || PID_MC==8 || PID_MC==9) && isinECAL_MC==1 && %f<xh_MC && xh_MC<%f && %f<yh_MC && yh_MC<%f",x1,x2,y1,y2));
+  h3 = (TH1F*) gDirectory->Get("h3");
+  picount = RK*h3->GetEntries();
+
+  TH1F *h4 = new TH1F();
+  HadronMC->Draw("(EECAL1_MC+EECAL2_MC)/phad_MC>>h4",Form("(PID_MC==0 || PID_MC==1 || PID_MC==8 || PID_MC==9) && isinECAL_MC==1 && 0.8<(EECAL1_MC+EECAL2_MC)/phad && (EECAL1_MC+EECAL2_MC)/phad_MC<1.25 && %f<xh_MC && xh_MC<%f && %f<yh_MC && yh_MC<%f",x1,x2,y1,y2));
+  h4 = (TH1F*) gDirectory->Get("h4");
+  piecount = h4->GetEntries();
+
+  econt = h3->GetEntries() ? (piecount - picount)/h3->GetEntries() : 0;
+
+  return econt;
+}
+
 void contaminationTable()
 {
 
@@ -44,6 +79,22 @@ void contaminationTable()
     for(int j=0; j<5; j++)
     {
       cout << contamination(fXrange[i], fXrange[i+1], fYrange[j], fYrange[j+1]) << " ";
+    }
+    cout << endl;
+  }
+}
+
+void contaminationTableMC()
+{
+
+  double fXrange[10] = {.004,.01,.02,.03,.04,.06,.1,.14,.18,.4};
+  double fYrange[6] = {.1,.15,.2,.3,.5,.7};
+
+  for(int i=0; i<9; i++)
+  {
+    for(int j=0; j<5; j++)
+    {
+      cout << contaminationMC(fXrange[i], fXrange[i+1], fYrange[j], fYrange[j+1]) << " ";
     }
     cout << endl;
   }
