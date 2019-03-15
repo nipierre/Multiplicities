7cc61a80730a29334bff26842045d453dfafc8b1
npierre <nicolas.pierre@cern.ch>
2019-03-15 11:33:08 +0100

    Fix.

--- ElectronMacro.C
+++ ElectronMacro.C
@@ -9,18 +9,27 @@ double contamination(double x1, double x2)
   TFile *_file0 = TFile::Open("RDMC.root");
   TTree *Hadron = (TTree*) _file0->Get("Hadron");
 
-  TH1F *h1, *h2, *h3, *h4;
-
+  TH1F *h1 = new TH1F();
   Hadron->Draw("(EECAL1+EECAL2)/phad>>h1",Form("(PID==2 || PID==3) && isinECAL==1 && %f<xh && xh<%f",x1,x2));
-  // RK = h1->GetEntries();
-  // Hadron->Draw("(EECAL1+EECAL2)/phad>>h2",Form("(PID==2 || PID==3) && isinECAL==1 && 0.8<(EECAL1+EECAL2)/phad && (EECAL1+EECAL2)/phad<1.25 && %f<xh && xh<%f",x1,x2));
-  // RK /= h2->GetEntries();
-  // Hadron->Draw("(EECAL1+EECAL2)/phad>>h3",Form("(PID==0 || PID==1) && isinECAL==1 && %f<xh && xh<%f",x1,x2));
-  // picount = RK*h3->GetEntries();
-  // Hadron->Draw("(EECAL1+EECAL2)/phad>>h4",Form("(PID==0 || PID==1) && isinECAL==1 && 0.8<(EECAL1+EECAL2)/phad && (EECAL1+EECAL2)/phad<1.25 && %f<xh && xh<%f",x1,x2));
-  // piecount = h4->GetEntries();
-  //
-  // econt = (piecount - picount)/h3->GetEntries();
+  h1 = (TH1F*) gDirectory->Get("h1");
+  RK = h1->GetEntries();
+
+  TH1F *h2 = new TH1F();
+  Hadron->Draw("(EECAL1+EECAL2)/phad>>h2",Form("(PID==2 || PID==3) && isinECAL==1 && 0.8<(EECAL1+EECAL2)/phad && (EECAL1+EECAL2)/phad<1.25 && %f<xh && xh<%f",x1,x2));
+  h2 = (TH1F*) gDirectory->Get("h2");
+  RK /= h2->GetEntries();
+
+  TH1F *h3 = new TH1F();
+  Hadron->Draw("(EECAL1+EECAL2)/phad>>h3",Form("(PID==0 || PID==1) && isinECAL==1 && %f<xh && xh<%f",x1,x2));
+  h3 = (TH1F*) gDirectory->Get("h3");
+  picount = RK*h3->GetEntries();
+
+  TH1F *h4 = new TH1F();
+  Hadron->Draw("(EECAL1+EECAL2)/phad>>h4",Form("(PID==0 || PID==1) && isinECAL==1 && 0.8<(EECAL1+EECAL2)/phad && (EECAL1+EECAL2)/phad<1.25 && %f<xh && xh<%f",x1,x2));
+  h4 = (TH1F*) gDirectory->Get("h4");
+  piecount = h4->GetEntries();
+
+  econt = (piecount - picount)/h3->GetEntries();
 
   return econt;
 }
