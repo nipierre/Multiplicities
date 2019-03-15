b39a46ef3d28a2b57f795504cbc1833f854bdbe1
npierre <nicolas.pierre@cern.ch>
2019-03-15 16:26:41 +0100

    Fix.

--- FitElectron.C
+++ FitElectron.C
@@ -3,8 +3,12 @@ double FitElectron(double x1, double x2)
   TFile *_file0 = TFile::Open("RDMC.root");
   TTree *Hadron = (TTree*) _file0->Get("Hadron");
 
+  TH1F *h0 = new TH1F();
+  Hadron->Draw("(EECAL1+EECAL2)/phad>>h0",Form("(PID==0 || PID==1) && isinECAL==1 && %f<xh && xh<%f",x1,x2));
+  h0 = (TH1F*) gDirectory->Get("h0");
+
   TH1F *h1 = new TH1F();
-  Hadron->Draw("(EECAL1+EECAL2)/phad>>h1",Form("(PID==0 || PID==1) && isinECAL==1 && %f<xh && xh<%f",x1,x2));
+  Hadron->Draw("(EECAL1+EECAL2)/phad>>h1",Form("(PID==0 || PID==1) && isinECAL==1 && 0.1<(EECAL1_MC+EECAL2_MC)/phad_MC && (EECAL1_MC+EECAL2_MC)/phad_MC<1.25 && %f<xh && xh<%f",x1,x2));
   h1 = (TH1F*) gDirectory->Get("h1");
 
   TF1 *f1 = new TF1("f1","pol3+gaus(4)",0.3,1.25);
@@ -26,7 +30,7 @@ double FitElectron(double x1, double x2)
   double sigma = f1->GetParameter(6);
   double norm = f1->GetParameter(4);
   double integral = 2*sigma*norm*sqrt(TMath::Pi());
-  double econt = integral/h1->GetEntries();
+  double econt = integral/h0->GetEntries();
 
   return econt;
 }
@@ -34,9 +38,14 @@ double FitElectron(double x1, double x2)
 double ElectronTable()
 {
   double fXrange[10] = {.004,.01,.02,.03,.04,.06,.1,.14,.18,.4};
+  double cont[9];
 
   for(int i=0; i<9; i++)
   {
-      cout << FitElectron(fXrange[i], fXrange[i+1]) << " ";
+      cont[i] = FitElectron(fXrange[i], fXrange[i+1]);
+  }
+  for(int i=0; i<9; i++)
+  {
+      cout << cont[i] << " ";
   }
 }
