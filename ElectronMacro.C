e594ca322b9a716b989320c68b514c54e1279eca
npierre <nicolas.pierre@cern.ch>
2019-03-15 11:21:16 +0100

    Fix.

--- ElectronMacro.C
+++ ElectronMacro.C
@@ -9,6 +9,8 @@ double contamination(double x1, double x2)
   TFile *_file0 = TFile::Open("RDMC.root");
   TTree *Hadron = (TTree*) _file0->Get("Hadron");
 
+  TH1F *h1, *h2, *h3, *h4;
+
   Hadron->Draw("(EECAL1+EECAL2)/phad>>h1",Form("(PID==2 || PID==3) && isinECAL==1 && %f<xh && xh<%f",x1,x2));
   RK = h1->GetEntries();
   Hadron->Draw("(EECAL1+EECAL2)/phad>>h2",Form("(PID==2 || PID==3) && isinECAL==1 && 0.8<(EECAL1+EECAL2)/phad && (EECAL1+EECAL2)/phad<1.25 && %f<xh && xh<%f",x1,x2));
