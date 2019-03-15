d556340b19cc925fce32780aa3f226bd11eda8ab
npierre <nicolas.pierre@cern.ch>
2019-03-15 16:27:48 +0100

    Fix.

--- FitElectron.C
+++ FitElectron.C
@@ -8,7 +8,7 @@ double FitElectron(double x1, double x2)
   h0 = (TH1F*) gDirectory->Get("h0");
 
   TH1F *h1 = new TH1F();
-  Hadron->Draw("(EECAL1+EECAL2)/phad>>h1",Form("(PID==0 || PID==1) && isinECAL==1 && 0.1<(EECAL1+EECAL2)/phad_MC && (EECAL1+EECAL2)/phad_MC<1.25 && %f<xh && xh<%f",x1,x2));
+  Hadron->Draw("(EECAL1+EECAL2)/phad>>h1",Form("(PID==0 || PID==1) && isinECAL==1 && 0.1<(EECAL1+EECAL2)/phad && (EECAL1+EECAL2)/phad<1.25 && %f<xh && xh<%f",x1,x2));
   h1 = (TH1F*) gDirectory->Get("h1");
 
   TF1 *f1 = new TF1("f1","pol3+gaus(4)",0.3,1.25);
