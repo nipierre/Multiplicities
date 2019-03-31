c2144366956718429ce24742ace9f9af5a12c0c6
npierre <nicolas.pierre@cern.ch>
2019-03-31 14:43:22 +0200

    Fix.

--- analySIDIS_collect.cc
+++ analySIDIS_collect.cc
@@ -4353,8 +4353,8 @@ int main(int argc, char **argv)
                   << fMultiplicities[i][j][k].tab[0][0][0] << " " << fMultiplicities[i][j][k].tab[0][1][0] << " "
                   << fAcceptance[6][i][j][k].tab[1][1][0][0] << " " << fAcceptance[6][i][j][k].tab[1][1][1][0] << " "
                   << fAcceptance[6][i][j][k].tab[0][0][0][0] << " " << fAcceptance[6][i][j][k].tab[0][0][1][0] << " "
-                  << fBinning_period[6][i][j][k].tab[1][1][0][0]+fBinning_period[6][i][j][k].tab[1][1][0][0] << fNDIS_evt_period[6][0][0][i][j][k]+fNDIS_evt_period[6][0][1][i][j][k] << " "
-                  << fBinning_period[6][i][j][k].tab[0][1][0][0]+fBinning_period[6][i][j][k].tab[0][1][0][0] << fNDIS_evt_period[6][0][0][i][j][k]+fNDIS_evt_period[6][0][1][i][j][k] << " "
+                  << fBinning_period[6][i][j][k].tab[1][1][0][0]+fBinning_period[6][i][j][k].tab[1][1][0][0] << " " << fNDIS_evt_period[6][0][0][i][j][k]+fNDIS_evt_period[6][0][1][i][j][k] << " "
+                  << fBinning_period[6][i][j][k].tab[0][1][0][0]+fBinning_period[6][i][j][k].tab[0][1][0][0] << " " << fNDIS_evt_period[6][0][0][i][j][k]+fNDIS_evt_period[6][0][1][i][j][k] << " "
                   << "0 0 0 0 0 0 0 0 "
                   << GetSemiInclusiveRadiativeCorrection(1,i,j,k+1) << " " << GetSemiInclusiveRadiativeCorrection(0,i,j,k+1) << " "
                   << fDiffVectorMeson[1][i][j][k][0] << " " << fDiffVectorMeson[1][i][j][k][0] << endl;
