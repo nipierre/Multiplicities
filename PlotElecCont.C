ca28951ea7a8f119c16d711545f90c0e33bfde2d
npierre <nicolas.pierre@cern.ch>
2019-07-08 15:45:36 +0200

    Fix.

--- PlotElecCont.C
+++ PlotElecCont.C
@@ -1,12 +1,7 @@
-void PlotMultComp()
+void PlotElecCont()
 {
   float dummy;
-  double fAcc1[2][9][6][12];
-  double fAcc1Err[2][9][6][12];
-  double fAcc2[2][9][6][12];
-  double fAcc2Err[2][9][6][12];
-  double fComp[2][9][6][12];
-  double fCompErr[2][9][6][12];
+  double fECont[2][9][6][12];
 
   Int_t fMarkerColor[6] = {2,95,209,226,4,221};
   Int_t fMarkerStyle[6][2] = {{24,20},{26,22},{25,21},{27,33},{28,34},{30,29}};
@@ -20,66 +15,27 @@ void PlotMultComp()
 
   c1.Divide(9,5,0,0);
 
-  TGraphErrors* H_acc[2][9][6];
+  TGraph* H_acc[2][9][6];
 
-  ifstream Acc1("multiplicities_proton_raw.txt");
-  for(int i=0; i<9; i++)
+  ifstream econt("electron.txt");
+  for(int c=0; c<2; c++)
   {
-    for(int j=0; j<6; j++)
+    for(int i=0; i<9; i++)
     {
-      for(int k=0; k<12; k++)
+      for(int j=0; j<6; j++)
       {
-        for(int l=0; l<7; l++)
+        for(int k=0; k<12; k++)
         {
-          Acc1 >> dummy;
+          econt >> fECont[c][i][j][k] >> dummy;
         }
-        // Acc1 >> fAcc1[1][i][j][k] >> fAcc1Err[1][i][j][k] >> dummy >> dummy >> dummy >> dummy;
-        Acc1 >> fAcc1[1][i][j][k] >> fAcc1Err[1][i][j][k] >> dummy >> dummy;
-        // cout << fAcc1[1][i][j][k] << " " << fAcc1Err[1][i][j][k] << " ";
-        for(int l=0; l<4; l++)
-        {
-          Acc1 >> dummy;
-        }
-        // Acc1 >> fAcc1[0][i][j][k] >> fAcc1Err[0][i][j][k] >> dummy >> dummy >> dummy >> dummy;
-        Acc1 >> fAcc1[0][i][j][k] >> fAcc1Err[0][i][j][k] >> dummy >> dummy;
-        // cout << fAcc1[0][i][j][k] << " " << fAcc1Err[0][i][j][k] << endl;
       }
     }
   }
-  Acc1.close();
+  econt.close();
 
-  ifstream Acc2("multiplicities_proton_raw_P07.txt");
-  for(int i=0; i<9; i++)
-  {
-    for(int j=0; j<6; j++)
-    {
-      for(int k=0; k<12; k++)
-      {
-        for(int l=0; l<7; l++)
-        {
-          Acc2 >> dummy;
-        }
-        // Acc2 >> fAcc2[1][i][j][k] >> fAcc2Err[1][i][j][k] >> dummy >> dummy >> dummy >> dummy;
-        Acc2 >> fAcc2[1][i][j][k] >> fAcc2Err[1][i][j][k] >> dummy >> dummy;
-        cout << fAcc2[1][i][j][k] << " " << fAcc2Err[1][i][j][k] << " ";
-        fComp[1][i][j][k] = fAcc2[1][i][j][k]/fAcc1[1][i][j][k];
-        fCompErr[1][i][j][k] = pow(fAcc2Err[1][i][j][k]/fAcc1[1][i][j][k],2)+pow(fComp[1][i][j][k]*fAcc1Err[1][i][j][k]/fAcc1[1][i][j][k],2);
-        for(int l=0; l<4; l++)
-        {
-          Acc2 >> dummy;
-        }
-        // Acc2 >> fAcc2[0][i][j][k] >> fAcc2Err[0][i][j][k] >> dummy >> dummy >> dummy >> dummy;
-        Acc2 >> fAcc2[0][i][j][k] >> fAcc2Err[0][i][j][k] >> dummy >> dummy;
-        cout << fAcc2[0][i][j][k] << " " << fAcc2Err[0][i][j][k] << endl;
-        fComp[0][i][j][k] = fAcc2[0][i][j][k]/fAcc1[0][i][j][k];
-        fCompErr[0][i][j][k] = pow(fAcc2Err[0][i][j][k]/fAcc1[0][i][j][k],2)+pow(fComp[0][i][j][k]*fAcc1Err[0][i][j][k]/fAcc1[0][i][j][k],2);
-      }
-    }
-  }
-  Acc2.close();
-
-  TLine l1(0.1,1.05,0.9,1.05);
-  TLine l2(0.1,0.95,0.9,0.95);
+  TLine l1(0.1,0.05,0.9,0.05);
+  TLine l2(0.1,0.1,0.9,0.1);
+  TLine l3(0.1,0.,0.9,0.);
 
   l1.SetLineStyle(3); l2.SetLineStyle(3);
 
@@ -95,6 +51,8 @@ void PlotMultComp()
 
         for(int k=0; k<12; k++)
         {
+          fECont[c][i][j][k]=1-fECont[c][i][j][k];
+          
           if((j==4 && k>4)
           || (i==0 && j==5 && k==3)
           || (i>4 && j==4 && k==5)
@@ -117,24 +75,23 @@ void PlotMultComp()
           || (i==0 && j<3)
           || (i==1 && j==0))
           {
-            fComp[c][i][j][k]=0;
+            fECont[c][i][j][k]=0;
           }
-          h_a.push_back(fComp[c][i][j][k]);
-          h_err.push_back(sqrt(fCompErr[c][i][j][k]));
+          h_a.push_back(fECont[c][i][j][k]);
 
           z_range_h.push_back(z_range[k]);
         }
 
         for(int k=12; k>0; k--)
         {
-          if(!h_a[k-1]) {h_a.erase(h_a.begin()+k-1); h_err.erase(h_err.begin()+k-1); z_range_h.erase(z_range_h.begin()+k-1);}
+          if(!h_a[k-1]) {h_a.erase(h_a.begin()+k-1); z_range_h.erase(z_range_h.begin()+k-1);}
         }
 
         bool h_a_empty = 0;
 
         if(!(int(h_a.size()))) h_a_empty = 1;
 
-        H_acc[c][i][j] = new TGraphErrors(int(h_a.size()),&(z_range_h[0]),&(h_a[0]),0,&(h_err[0]));
+        H_acc[c][i][j] = new TGraphErrors(int(h_a.size()),&(z_range_h[0]),&(h_a[0]));
 
         if(!c)
         {
@@ -165,8 +122,8 @@ void PlotMultComp()
             {
               H_acc[c][i][j]->Draw("SAMEPA");
               H_acc[c][i][j]->GetXaxis()->SetLimits(0.1,0.9);
-              H_acc[c][i][j]->SetMinimum(0.45);
-              H_acc[c][i][j]->SetMaximum(1.55);
+              H_acc[c][i][j]->SetMinimum(-0.01);
+              H_acc[c][i][j]->SetMaximum(0.09);
               H_acc[c][i][j]->GetXaxis()->SetLabelSize(0.06);
               H_acc[c][i][j]->GetYaxis()->SetLabelSize(0.06);
               H_acc[c][i][j]->SetTitle("");
@@ -182,23 +139,23 @@ void PlotMultComp()
               H_acc[c][i][j]->GetYaxis()->SetNdivisions(304,kTRUE);
               if(i==1 && j==0)
               {
-                H_acc[c][i][j]->GetYaxis()->SetTitle("#font[12]{acceptance}^{#font[ 12]{h}}");
+                H_acc[c][i][j]->GetYaxis()->SetTitle("#font[12]{e}^{#font[ 12]{#pi}}");
                 H_acc[c][i][j]->GetYaxis()->SetTitleSize(0.08);
               }
               H_acc[c][i][j]->Draw("SAMEP");
-              l1.Draw("SAME"); l2.Draw("SAME");
+              l1.Draw("SAME"); l2.Draw("SAME"); l3.Draw("SAME");
               H_acc[c][i][j]->GetXaxis()->SetLimits(0.1,0.9);
-              H_acc[c][i][j]->SetMinimum(0.45);
-              H_acc[c][i][j]->SetMaximum(1.55);
+              H_acc[c][i][j]->SetMinimum(-0.01);
+              H_acc[c][i][j]->SetMaximum(0.09);
               c1.Range(0.1,0.,0.9,1.2);
             }
             else
             {
               H_acc[c][i][j]->Draw("SAMEP");
-              l1.Draw("SAME"); l2.Draw("SAME");
+              l1.Draw("SAME"); l2.Draw("SAME"); l3.Draw("SAME");
               H_acc[c][i][j]->GetXaxis()->SetLimits(0.1,0.9);
-              H_acc[c][i][j]->SetMinimum(0.45);
-              H_acc[c][i][j]->SetMaximum(1.55);
+              H_acc[c][i][j]->SetMinimum(-0.01);
+              H_acc[c][i][j]->SetMaximum(0.09);
             }
           }
           c1.Update();
@@ -207,5 +164,5 @@ void PlotMultComp()
     }
   }
 
-  c1.Print("Mult_comparison.pdf");
+  c1.Print("Electron_contamination.pdf");
 }
